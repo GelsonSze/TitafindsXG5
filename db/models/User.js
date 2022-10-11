655ff0b38c009fa7aacf010d7295f3b91666c316
Kaye11037 <82099511+Kaye11037@users.noreply.github.com>
2022-10-11 11:35:18 +0800

    Created User.js
    I made a user schema and added the basic characteristics of an account such as email, password, name, and whether one is admin or not.

---
+++ db/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    /*
+        Database schema for the user.
+    */
+    id:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password:{
+        type: String,
+        required: true,
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    firstName:{
+        type: String,
+        required: true,
+    },
+
+    lastName:{
+        type: String,
+        required: true,
+    },
+
+    isAdmin:{
+        type: Boolean,
+    },
+
+});
+
+export default mongoose.model("User", userSchema);
