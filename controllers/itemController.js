d80995ffbfc12a8c28e3e968ad23c9abcb729601
erik-lance <eriklance@gmail.com>
2022-10-25 20:17:05 +0800

    PMS-46 Added override file for w2ui table

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -6,7 +6,7 @@ const itemController = {
     getIndex: function(req, res){
         res.render('index', {
             title: 'index',
-            styles: ['index.css'],
+            styles: ['index.css','w2ui-overrides.css'],
             scripts: ['index.js']
         });
     },
