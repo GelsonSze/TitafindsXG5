408277255455290083e525835ad71d5e84b6d1ec
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-10-31 21:31:06 +0800

    * removed created folder

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import itemController from "../controllers/itemController.js";
 import userController from "../controllers/userController.js";
 import { checkAuth, checkNoAuth } from "../controllers/authController.js";
+import { generateItemCode } from "../utils/helper.js";
 
 const app = express();
 
@@ -26,12 +27,63 @@ if (process.env.NODE_ENV === "development") {
     console.log("Development mode: Adding sample items to database");
     var samples= [
         { 
-            
-
+            image: "test.png",
+            code: generateItemCode("Necklace"),
+            name: "Phoenix Necklace",
+            type: "Necklace",
+            brand: "Cartier",
+            classification: "14 karat",
+            design: "regular",
+            size: 16,
+            weight: 10,
+            quantity: 100,
+            sellingType: "per gram",
+            purchasePrice: 5000,
+            sellingPrice: 10000,
+            status: "Available",
+            dateAdded: "10/1/2022",
+            dateUpdated: "10/31/2022",
+            addedBy: "admin"
+        },
+        { 
+            image: "test.png",
+            code: generateItemCode("Necklace"),
+            name: "Saudi Gold Tiffany Necklace",
+            type: "Necklace",
+            brand: "Tiffany",
+            classification: "18 karat",
+            design: "regular",
+            size: 18,
+            weight: 20,
+            quantity: 140,
+            sellingType: "per gram",
+            purchasePrice: 8000,
+            sellingPrice: 18000,
+            status: "Available",
+            dateAdded: "10/2/2022",
+            dateUpdated: "10/31/2022",
+            addedBy: "admin"
+        },
+        { 
+            image: "test.png",
+            code: generateItemCode("Chain"),
+            name: "VVsplChristian Dior Saudi Gold Cadena Chain",
+            type: "Chain",
+            brand: "Swarovski",
+            classification: "18 karat",
+            design: "regular",
+            size: 22,
+            weight: 18,
+            quantity: 0,
+            sellingType: "per gram",
+            purchasePrice: 20000,
+            sellingPrice: 40000,
+            status: "Unvailable",
+            dateAdded: "10/21/2022",
+            dateUpdated: "10/31/2022",
+            addedBy: "admin"
         }
-
     ]
-
     itemController.addItemSamples(samples);
 }
 
