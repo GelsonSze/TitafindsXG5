950d468af9181ebd54adaf9f2e4f84d7814f204c
aaron <aaron_palpallatoc@dlsu.edu.ph>
2022-11-21 20:46:45 +0800

    * initial sidebar setup * package-json: formatting, added css ext

--- public/js/base.js
+++ public/js/base.js
@@ -15,6 +15,34 @@ const tags = [
     { name: "Jobs", color: "brown" },
 ];
 
+$(function () {
+    let sidebar = document.querySelector(".sidebar");
+    let closeBtn = document.querySelector("#btn");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+        menuBtnChange();
+    });
+
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e) => {
+            let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+            arrowParent.classList.toggle("showMenu");
+        });
+
+        // following are the code to change sidebar button(optional)
+        function menuBtnChange() {
+            if (sidebar.classList.contains("close")) {
+                closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+            } else {
+                closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+            }
+        }
+    }
+});
+
 // var arrowDown,
 //     profileActive = 0,
 //     profileDropdown;
