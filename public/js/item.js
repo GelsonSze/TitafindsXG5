867bfd4e5d1e80cbb1c3c81a87293094939247a8
JulianneAndrea <83597151+julianneviz@users.noreply.github.com>
2022-11-25 23:36:52 +0800

    PMS-98 delete and edit button

--- public/js/item.js
+++ public/js/item.js
@@ -174,25 +174,6 @@ function itemDesc(field, desc) {
     return html;
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches(".dropbtn")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");
-            }
-        }
-    }
-};
-
 $(document).ready(function () {
     // Loads item for the page.
     getItem();
