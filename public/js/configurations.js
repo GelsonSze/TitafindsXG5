44a0fab81ea3aefbc6513f10c1cc15f72972105b
erik-lance <eriklance@gmail.com>
2022-11-29 14:47:35 +0800

    PMS-154 W2UI table loaded

--- public/js/configurations.js
+++ public/js/configurations.js
@@ -1,5 +1,3 @@
-import { w2ui, w2layout, w2sidebar } from "w2ui.es6.min.js"
-
 let config = {
     layout: {
         name: 'layout',
@@ -31,26 +29,47 @@ let config = {
     },
 }
 
-let layout = new w2layout(config.layout)
-let sidebar = new w2sidebar(config.sidebar)
+// let layout = new w2layout(config.layout)
+// let sidebar = new w2sidebar(config.sidebar)
 // // initialization
-layout.render('#config-attrib-grid')
-layout.html('left', sidebar)
+// layout.render('#config-attrib-grid')
+// layout.html('left', sidebar)
 
 
 
 $(function() {
-    // $('#config-attrib-grid').w2layout({
-    //     name: 'layout',
-    //     padding: 0,
-    //     panels: [
-    //         { type: 'left', size: 200, resizable: true, minSize: 120 },
-    //         { type: 'main', minSize: 550, overflow: 'hidden' }
-    //     ],
-    // })
+    $('#config-attrib-grid').w2layout({
+        name: 'config-attrib-grid',
+        padding: 0,
+        panels: [
+            { type: 'left', size: 200, resizable: true, minSize: 120 },
+            { type: 'main', minSize: 550, overflow: 'hidden' }
+        ],
+    })
 
-    // //let sidebar = new w2ui.w2sidebar(config.sidebar)
+    $('#config-attrib-grid-sidebar').w2sidebar({
+        name: 'attrib-sidebar',
+        nodes: [
+            { id: 'general', text: 'General', group: true, expanded: true, nodes: [
+                { id: 'html', text: 'Some HTML', icon: 'fa fa-list-alt' }
+            ]}
+        ],
+        onClick(event) {
+            switch (event.target) {
+                case 'html':
+                    layout.html('main', '<div style="padding: 10px">Some HTML</div>')
+                    query(layout.el('main'))
+                        .removeClass('w2ui-grid')
+                        .css({
+                            'border-left': '1px solid #efefef'
+                        })
+                    break
+            }
+        }
+    })
 
-    // w2ui["config-attrib-grid"].html('left', sidebar)
+    //let sidebar = new w2ui.w2sidebar(config.sidebar)
+
+    w2ui["config-attrib-grid"].html('left', w2ui["attrib-sidebar"])
     
 })
