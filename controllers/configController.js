66edcf4851bd511b9acc4d7de270da6def0c1870
erik-lance <eriklance@gmail.com>
2022-12-04 22:02:59 +0800

    PMS-157 Set up attribute DB

--- controllers/configController.js
+++ controllers/configController.js
@@ -13,7 +13,42 @@ const configController = {
         })
     },
 
+    addAttribute: async function (req, res) {
+            try {
+                var newAttrib = {
+                    name: req.body.name,
+                    dataType: req.body.type,
+                    options: req.body.options,
+                };
 
+                console.log("Added attribute: ");
+                console.log(newAttrib);
+
+                db.insertOne(Attribute, newAttrib, function (data) {
+                    res.send(data);
+                });
+            } catch (error) {
+                res.status(500).json({
+                    message: "Server Error: Add Attribute",
+                    details: error.message,
+                });
+                return;
+            }
+    },
+
+    getAttributes: async function (req, res) {
+        try {
+            db.findMany(Attribute, {}, null, function (data) {
+                res.status(200).json(data);
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Server Error: Get Attributes",
+                details: error.message,
+            });
+            return;
+        }
+    }
 
 };
 
